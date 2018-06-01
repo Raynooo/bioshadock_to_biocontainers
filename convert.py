cb10920799b7f76dd4d025b9d82d0b6e22e1cce6
Raynooo <renaud.jullien@gmail.com>
2018-06-01 16:15:34 +0000

    Dockerfiles are now pulled from their repo

--- convert.py
+++ convert.py
@@ -6,17 +6,39 @@ from pprint import pprint
 import sys
 import os
 import re
+import requests
 
 from specs import *
 
+BLACKLIST=re.compile("^(bioconda|debian)")
+TOOLSPATH="https://docker-ui.genouest.org/container/all?light=true"
+DOCKERFILESROOt="https://docker-ui.genouest.org/container/"
+
+##############Acquiring the original Dockerfiles#################
+
+def get_tools_ids (url,ignoreRegex):
+    r = requests.get (url)
+    myTools=list()
+
+    for crt_entry in r.json():
+        if crt_entry.get("visible"):
+            if not ignoreRegex.search(crt_entry.get("id")):
+                if crt_entry.get("meta").get("Dockerfile"):
+                    myTools.append(crt_entry.get("id"))
+
+    #print myTools
+    #print len(myTools)
+    return myTools
+
+def get_tool_dockerfile (url, id):
+    r = requests.get (url+id)
+    #print r.json().get("meta").get("Dockerfile")
+    return r.json().get("meta").get("Dockerfile")
+
+###########Treating them##############
+
 def treat_this_label (crtKey, crtVal):
-    if crtKey.endswith("description"):
-        print "Description found!"
-        return 1
-    elif crtKey.endswith("homepage"):
-        print "URL found!"
-        return 1
-    return 0
+    return 1
 
 def parsing_labels (crtLabels):
     #print type(crtLabels)
@@ -27,32 +49,30 @@ def parsing_labels (crtLabels):
         newPair = treat_this_label (key, crtLabels.get(key))
 
 
+#################MAIN#####################
 
 
+cmt_line_re = "^[\s]*#"
+
+#docker_file = None
+
+ids=get_tools_ids(TOOLSPATH, BLACKLIST)
+
+for crt_id in ids:
+    crt_Dockerfile=get_tool_dockerfile(DOCKERFILESROOt, crt_id)
+    #print crt_id
+    dfp = DockerfileParser()
+    dfp.content = crt_Dockerfile
+    #pprint (dfp.labels)
+    lines = crt_Dockerfile.split("\n")
+    for line in lines:
+        if re.search(cmt_line_re, line):
+            print line
+            #TODO: search comment line for metadata
 
 
+    #pprint (dfp.json)
+    #print dfp.labels
+    #pprint (dfp.structure)
+    #parsing_labels (dfp.labels)
 
-
-docker_file = None
-if len(sys.argv) > 1 and sys.argv[1]:
-    docker_file = sys.argv[1]
-
-if not docker_file or not os.path.exists(docker_file):
-    print("Could not find Dockerfile "+str(docker_file))
-    sys.exit(1)
-
-with open(docker_file, 'r') as content_file:
-    content = content_file.read()
-
-dfp = DockerfileParser()
-dfp.content = content
-labels = dfp.labels
-
-#pprint (labels)
-#pprint (dfp.structure)
-#pprint (dfp.json)
-#print labels
-
-parsing_labels (labels)
-
-print SPECDICT
